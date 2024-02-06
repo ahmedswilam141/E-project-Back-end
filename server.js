52836b5b70ad74034a6132262a502af2c7dca292
ahmedswilam141 <ahmedswelam141@gmail.com>
2024-02-06 19:19:35 +0200

    Done(4)

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+
 dotenv.config({ path: 'config.env' });
+const dbConnection = require('./config/database');
+const categoryRoute = require('./routes/categoryRoute');
 
-mongoose.connect(process.env.DB_URI).then((conn) => {
-    console.log(`Database Connected: ${conn.connection.host}`);
-}).catch((err) => {
-    console.error(`Database Error: ${err}`);
-    process.exit(1);
-});
-
+// Connect with db
+dbConnection();
 
 // express app
 const app = express();
@@ -18,42 +15,15 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
     console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-// 1- Create Schema
-const categorySchema = new mongoose.Schema({
-    name: String,
-});
-
-// 2- Create model
-const CategoryModel = mongoose.model('Category', categorySchema);
-
-
-// Routes
-app.post('/', (req, res) => {
-    const name = req.body.name;
-    console.log(req.body);
-
-    const newCategory = new CategoryModel({ name });
-    newCategory
-        .save()
-        .then((doc) => {
-            res.json(doc);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
-
-});
-
-app.get('/', (req, res) => {
-    res.send('Ahmed Malek');
-});
+// Mount Routes
+app.use('/api/v1/categories', categoryRoute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
+    console.log(`App running running on port ${PORT}`);
 });
